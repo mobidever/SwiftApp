e9f4c9e3bb80871598ac3504b89d3a00974d92ac
mobidever <pramod.mobidev@gmail.com>
2024-03-30 23:47:04 +0530

    Updated Live Statistics Data
    Updated Live Statistics Data

--- SampleSwiftUI/Core/Home/ViewModels/HomeViewModel.swift
+++ SampleSwiftUI/Core/Home/ViewModels/HomeViewModel.swift
@@ -10,16 +10,17 @@ import Combine
 
 class HomeViewModel: ObservableObject {
 	
-	private let dataService = CoinDataService()
+	private let coinDataService = CoinDataService()
+	private let marketDataService = MarketDataService()
 	
 	
-	@Published var  statistics: [StatisticModel] = [
-		StatisticModel(title: "title", value: "value", percentageChange: 1),
-		StatisticModel(title: "title", value: "value"),
-		StatisticModel(title: "title", value: "value"),
-		StatisticModel(title: "title", value: "value", percentageChange: -7)
-		
-	]
+	@Published var  statistics: [StatisticModel] = []
+//		StatisticModel(title: "title", value: "value", percentageChange: 1),
+//		StatisticModel(title: "title", value: "value"),
+//		StatisticModel(title: "title", value: "value"),
+//		StatisticModel(title: "title", value: "value", percentageChange: -7)
+//		
+//	]
 	@Published var allCoins: [CoinModel] = []
 	@Published var portfolioCoins: [CoinModel] = []
 	
@@ -42,7 +43,13 @@ class HomeViewModel: ObservableObject {
 //			}
 //			.store(in: &cancellables)
 		
-		$searchText.combineLatest(dataService.$allCoins)
+		marketDataService.$marketData
+			.map(mapGlobalMarketData)
+			.sink { [weak self] (returnedStats) in
+			self?.statistics = returnedStats
+		}.store(in: &cancellables)
+		
+		$searchText.combineLatest(coinDataService.$allCoins)
 			.debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
 			.map(filterCoins)
 			.sink { [weak self] (returnedCoins) in
@@ -50,6 +57,21 @@ class HomeViewModel: ObservableObject {
 			}
 			.store(in: &cancellables)
 			
+	}
+	
+	private func mapGlobalMarketData(marketDataModel: MarketDataModel?) -> [StatisticModel] {
+		var stats : [StatisticModel] = []
+		guard let data = marketDataModel else {
+			return stats
+		}
+		let marketCap = StatisticModel(title: "Market Cap", value: data.marketCap, percentageChange: data.marketCapChangePercentage24HUsd)
+		let volume = StatisticModel(title: "Volume", value: data.volume)
+		let btcDominance = StatisticModel(title: "BTC Dominance", value: data.btcDominance)
+		let portfolio = StatisticModel(title: "Portfolio Value", value: "$0.00", percentageChange: 0)
+		stats.append(contentsOf: [
+			marketCap, volume, btcDominance, portfolio
+		])
+		return stats
 	}
 	
 	private func filterCoins(text: String, coins: [CoinModel]) ->  [CoinModel] {
---
+++ SampleSwiftUI/Services/MarketDataService.swift
@@ -0,0 +1,43 @@
+//
+//  MarketDataService.swift
+//  SampleSwiftUI
+//
+//  Created by PS on 30/03/24.
+//
+
+import Foundation
+import Combine
+
+class MarketDataService {
+	
+	@Published var marketData: MarketDataModel? = nil
+		
+	var marketDataSubscription: AnyCancellable?
+	
+	init() {
+		getCoins()
+	}
+	
+	private func getCoins(){
+		
+		guard let url = URL(string: "https://api.coingecko.com/api/v3/global") else {
+			return
+		}
+		
+		marketDataSubscription = NetworkingManager.download(url: url)
+			.decode(type: GlobalData.self, decoder: JSONDecoder())
+			.sink { (completion) in
+				print("Inside Sink")
+				NetworkingManager.handleCompletion(completion: completion)
+				
+			} receiveValue: { [weak self] (returnedGlobalData ) in
+				print("Inside receivedValue")
+				self?.marketData = returnedGlobalData.data
+				self?.marketDataSubscription?.cancel()
+			}
+		
+		
+		
+		
+	}
+}
